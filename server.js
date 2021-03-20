69af5863c5405de5f799ac035906b18127f2ebd4
patrickmklee <patrickmklee93@gmail.com>
2021-03-19 21:16:43 -0700

    Add handlebars dependency, views directory

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+
+// Sets Handlebars as the default template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
